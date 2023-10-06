70e2a99643de16d235aacc36ad9898accfaf4316
lepleen <leticiachavescs@gmail.com>
2023-10-05 22:29:07 -0300

    update

--- blog_ang/src/app/pages/content/content.component.ts
+++ blog_ang/src/app/pages/content/content.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { dataFake } from '../../../data/dataFake'
 
 @Component({
   selector: 'app-content',
@@ -8,20 +9,29 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class ContentComponent implements OnInit {
 	@Input()
-	photoCover:string="https://uploads.jovemnerd.com.br/wp-content/uploads/2017/06/doctor-who-all-doctors.png"
+	photoCover:string=""
 	@Input()
-	contentTitle:string="What is Lorem Ipsum?"
+	contentTitle:string=""
 	@Input()
-	contentDescription:string="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+	contentDescription:string="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged."
+	private id:string | null="0"
 
   constructor(
 		private route:ActivatedRoute
 	) { }
 
   ngOnInit(): void {
-		this.route.paramMap.subscribe(value =>
-			console.log(value.get("id"))
+		this.route.paramMap.subscribe( value =>
+			this.id = value.get("id")
 		)
+		this.setValuesComponent(this.id)
   }
+
+	setValuesComponent(id:string | null){
+		const result = dataFake.filter(article =>  article.id == id)[0]
+		this.contentTitle = result.title
+		this.contentDescription = result.description
+		this.photoCover = result.photoCover
+	}
 
 }
--- blog_ang/src/data/dataFake.ts
+++ blog_ang/src/data/dataFake.ts
@@ -1,15 +1,46 @@
 export const dataFake = [
 	{
-	"id": "",
-	"photo":" teste",
-	"title": "teste teste teste",
-	"description": "teste teste teste",
+	"id": "1",
+	"title": "Nova temporada de doctor who",
+	"description": " Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. ",
+	"photoCover": "https://s2-techtudo.glbimg.com/veBvHGm6Gm0jZiaJlSnVZGV-UmE=/0x0:1200x700/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2022/b/B/EC6PgrRCCSAEaQmM65iw/doctor-who-2.jpg",
 	},
 
 	{
-		"id": "",
-		"photo":" teste",
-		"title": "teste teste teste",
-		"description": "teste teste teste",
+		"id": "2",
+		"title": "Que doutor você seria? faça o teste",
+		"description": " Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. ",
+		"photoCover": "https://uploads.jovemnerd.com.br/wp-content/uploads/2017/06/doctor-who-all-doctors.png",
+
+	},
+
+	{
+		"id": "3",
+		"title": "Novo template doctor who",
+		"description": " Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. ",
+		"photoCover": "https://static1.colliderimages.com/wordpress/wp-content/uploads/2022/04/Doctor-Who-Plot-Holes-feature-image.jpg",
+
+	},
+	{
+		"id": "4",
+		"title": "Especial de natal 2023, confira o trailer",
+		"description": " Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. ",
+		"photoCover": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQHC3I9ehSfD5ufxIwuabO_dF5HPfcIXDnWhQ&usqp=CAU",
+
+	},
+	{
+		"id": "5",
+		"title": "	Doctor who 2024 nova temporada novas aventuras.",
+		"description": " Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. ",
+		"photoCover": "https://cdn.ome.lt/Vj1C8Do-Wr6pARautPSKfseljAs=/1200x630/smart/extras/conteudos/doctorwholista.jpg",
+
+	},
+
+	{
+		"id": "6",
+		"title": "	Os episodios mais assustadores e os vilões mais temidos.",
+		"description": " Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. ",
+		"photoCover": "https://cdn1.epicgames.com/salesEvent/salesEvent/EGS_DoctorWhoTheLonelyAssassins_KaiganGames_S1_2560x1440-9456dba5cd9e8e05e7731085c7778fbb",
+
 	},
 ]
